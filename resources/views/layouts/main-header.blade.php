135880f271e22019d543d961fd9a6be3f658f4a0
mahroustamim <mahroustamim@gmail.com>
2024-10-04 22:09:11 +0300

    remove some icon from dashboard

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -17,7 +17,7 @@
 			</div>
 		</div>
 		<div class="main-header-right">
-			<ul class="nav">
+			{{-- <ul class="nav">
 				<li class="">
 					<div class="dropdown  nav-itemd-none d-md-flex">
 						<a href="#" class="d-flex  nav-item nav-link pl-0 country-flag1" data-toggle="dropdown" aria-expanded="false">
@@ -60,7 +60,7 @@
 						</div>
 					</div>
 				</li>
-			</ul>
+			</ul> --}}
 			<div class="nav nav-item  navbar-nav-right ml-auto">
 				<div class="nav-link" id="bs-example-navbar-collapse-1">
 					<form class="navbar-form" role="search">
@@ -77,7 +77,7 @@
 						</div>
 					</form>
 				</div>
-				<div class="dropdown nav-item main-header-message ">
+				{{-- <div class="dropdown nav-item main-header-message ">
 					<a class="new nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg><span class=" pulse-danger"></span></a>
 					<div class="dropdown-menu">
 						<div class="menu-header-content bg-primary text-right">
@@ -153,7 +153,7 @@
 							<a href="text-center">VIEW ALL</a>
 						</div>
 					</div>
-				</div>
+				</div> --}}
 				<div class="dropdown nav-item main-header-notification">
 					<a class="new nav-link" href="#">
 					<svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg><span class=" pulse"></span></a>
